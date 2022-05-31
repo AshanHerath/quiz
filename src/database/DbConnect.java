a2b52efa7e0466412f3f210806e3fbdeab399cf9
AshanHerath <87167360+AshanHerath@users.noreply.github.com>
2022-05-31 19:46:57 +0530

    Update quizDb DbConnect class

--- src/database/DbConnect.java
+++ src/database/DbConnect.java
@@ -8,11 +8,11 @@ import java.sql.SQLException;
 public class DbConnect {
     static Connection con;
 
-    private static String dbHost = "localhost";
+    private static String dbHost = "soyura.com";
     private static String port = "3306";
-    private static String dbName = "xpos_db_2";
-    private static String userName = "sheronfdo";
-    private static String password = "Jamith@5000";
+    private static String dbName = "quizDb";
+    private static String userName = "soyura";
+    private static String password = "Soyura@123";
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         connect();
@@ -20,7 +20,7 @@ public class DbConnect {
 
     public static void connect() throws ClassNotFoundException,SQLException{
         Class.forName("com.mysql.cj.jdbc.Driver");
-        con = DriverManager.getConnection("jdbc:mysql://"+dbHost+":"+port+"/"+dbName,userName,password); // database name = xpos_db_2
+        con = DriverManager.getConnection("jdbc:mysql://"+dbHost+":"+port+"/"+dbName,userName,password); // database name = quizDb
     }
 
     public static void pushToDB(String sql)throws ClassNotFoundException,SQLException{
