0874886a08b6acb16befa9211db903dfd7645661
sheronfdo <jamithsheron5@gmail.com>
2022-05-31 19:43:50 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
-public class Main {
-    public static void main(String[] args) {
+import database.DbConnect;
 
+import java.sql.Connection;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        DbConnect db = new DbConnect();
+        Connection conn = db.getDBConnection();
     }
 }
